3992006f43c6f81d5b09c65dc5cf7e8ea80a2bdf
scholtenskyle <35465569+scholtenskyle@users.noreply.github.com>
2024-12-08 12:49:36 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
-function augmentingPath(graph, start, end) {
-    return [];
+function augmentingPath(graph, start, end, path = [], prev = []) {
+    path.push(start);
+    if (start == end) {
+        return path; 
+    }
+    prev.push(start);
+    if (graph[start]) {
+        for (var value in graph[start]) {
+            if (!prev.includes(value)) {
+                var newPath = augmentingPath(graph, value, end, path, visited);
+                if (newPath.length > 0) {
+                    return newPath;
+                }
+            }
+        }
+    }
+    path.pop();
+    prev.pop()
+    return [];  
 }
